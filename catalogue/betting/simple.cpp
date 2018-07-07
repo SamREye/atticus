e0fb420294080fe936e15b47d79b1297102ff4de
Samuel Bourque <sam.bourque@gmail.com>
2018-07-07 10:33:18 +0000

    Skeleton code

--- catalogue/betting/simple.cpp
+++ catalogue/betting/simple.cpp
@@ -1,55 +1,19 @@
 #include "simple.hpp"
 
-namespace bet_simple {
+namespace eosio {
 
-struct impl {
-    /**
-     * @brief Action to create new bet
-     */
-    void on(const create& c) {
-        require_auth(c.host);
-        eosio_assert(c.challenger != c.host, "challenger shouldn't be the same as host");
-        eosio::print("Bet created by ", c.host, "--waiting for challenger");
-    }
-
-    /**
-     * @brief Action to create new bet
-     */
-    void on(const accept& a) {
-        require_auth(a.challenger);
-        //eosio_assert()
-        eosio::print("Bet is accepted by ", a.challenger);
-    }
-
-    /**
-     * @brief Action to create new bet
-     */
-    void on(const declare& d) {
-        require_auth(d.expert);
-        eosio::print(d.winner, " has been declared the winner");
-    }
-
-    void apply( uint64_t /*receiver*/, uint64_t code, uint64_t action ) {
-        if (code == code_account) {
-            if (action == N(create)) {
-                impl::on(eosio::unpack_action_data<bet_simple::create>());
-            } else if (action == N(pledge)) {
-                impl::on(eosio::unpack_action_data<bet_simple::accept>());
-            } else if (action == N(declare)) {
-                impl::on(eosio::unpack_action_data<bet_simple::declare>());
-            }
-        }
-    }
-};
-
+void betsimple::create() {
+	eosio::print("CREATE");
 }
 
-extern "C" {
+void betsimple::accept() {
+	eosio::print("ACCEPT");
+}
 
-   using namespace bet_simple;
-   /// The apply method implements the dispatch of events to this contract
-   void apply( uint64_t receiver, uint64_t code, uint64_t action ) {
-      impl().apply(receiver, code, action);
-   }    
+void betsimple::declare() {
+	eosio::print("DECLARE");
+}
 
-} // extern "C"
+} /// namespace eosio
+
+EOSIO_ABI( eosio::betsimple, (create)(accept)(declare) )
--- catalogue/betting/simple.hpp
+++ catalogue/betting/simple.hpp
@@ -1,58 +1,15 @@
-/**
- *  @file
- *  @copyright defined in eos/LICENSE.txt
- */
 #pragma once
-
 #include <eosiolib/eosio.hpp>
-#include <eosiolib/asset.hpp>
 
-namespace bet_simple {
-        static const account_name master = N(bet.simple);
-        static const account_name code_account = N(bet.simple);
+namespace eosio {
 
-        struct bet_t {
-                account_name host;
-                account_name challenger;
-                account_name expert;
-                eosio::asset amount;
+   class betsimple : public contract {
+      public:
+         betsimple( account_name self ):contract(self){}
 
-                EOSLIB_SERIALIZE(bet_t, (host)(challenger)(expert)(amount))
-        };
-        
-	/**
-	 * @brief Action to create new game
-	*/
-	struct create {
-                account_name host;
-                account_name challenger;
-                account_name expert;
-                eosio::asset amount;
-                
-                EOSLIB_SERIALIZE(create, (host)(challenger)(expert)(amount))
-        };
-        
-        /**
-         * @brief Action to create new game
-        */
-        struct accept {
-                account_name host;
-                account_name challenger;
-                eosio::asset amount;
-                
-                EOSLIB_SERIALIZE(accept, (host)(challenger)(amount))
-        };
-        
-        /**
-         * @brief Action to create new game
-        */
-        struct declare {
-                account_name host;
-                account_name expert;
-                account_name winner;
-                
-                EOSLIB_SERIALIZE(declare, (host)(expert)(winner))
-        };
+         void create();
+         void accept();
+         void declare();
+   };
 
-        typedef eosio::multi_index<master, bet_t> bet;
-};
+} /// namespace eosio
