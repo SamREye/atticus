4dedfec39f7bad14cad4510fcd35f76274e661fa
Sam Bourque <sam.bourque@gmail.com>
2018-07-20 09:50:39 +0000

    Added basic forms

--- client/app/__init__.py
+++ client/app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask, Response
-
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-@app.route("/")
-def index():
-    return Response("It works!"), 200
+from app import routes, models
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ client/app/routes.py
@@ -0,0 +1,13 @@
+from flask import render_template, Response
+from app import app
+from app.forms import LoginForm
+
+@app.route("/")
+def index():
+    return Response("It works!"), 200
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+
