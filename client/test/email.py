2c554248af2f18f2050f677cb168b186c60c83aa
Sam Bourque <sam.bourque@gmail.com>
2018-07-21 06:47:26 +0000

    Integrated email

---
+++ client/test/email.py
@@ -0,0 +1,3 @@
+from app.email import send_email
+send_email('Testing... 1, 2, 3', sender='sb.themis@gmail.com', recipients=['david.klan@gmail.com', 'sam.bourque@gmail.com'], 'Test body', '<h1>Test HTML</h1>')
+
