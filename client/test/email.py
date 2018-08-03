9769c83c4095c0ebb49d6cb0a91fe351e346024c
Sam Bourque <sam.bourque@gmail.com>
2018-08-03 13:18:47 +0000

    Updated email for new domain

--- client/test/email.py
+++
@@ -1,3 +0,0 @@
-from app.email import send_email
-send_email('Testing... 1, 2, 3', sender='sb.themis@gmail.com', recipients=['david.klan@gmail.com', 'sam.bourque@gmail.com'], 'Test body', '<h1>Test HTML</h1>')
-
